41034741d12bf567ac1b442ce37efc6b3b65e12a
Qiu <3448658268@qq.com>
2025-09-27 21:17:06 +0800

    review

--- kernel/kalloc.c
+++ kernel/kalloc.c
@@ -21,12 +21,16 @@ struct run {
 struct {
   struct spinlock lock;
   struct run *freelist;
-} kmem;
+  char lock_name[7];
+} kmem[NCPU];
 
 void
 kinit()
 {
-  initlock(&kmem.lock, "kmem");
+  for (int i = 0; i < NCPU; i++) {
+    snprintf(kmem[i].lock_name, sizeof(kmem[i].lock_name), "kmem_%d", i);
+    initlock(&kmem[i].lock, kmem[i].lock_name);
+  }
   freerange(end, (void*)PHYSTOP);
 }
 
@@ -46,7 +50,8 @@ freerange(void *pa_start, void *pa_end)
 void
 kfree(void *pa)
 {
-  struct run *r;
+ 
+   struct run *r;
 
   if(((uint64)pa % PGSIZE) != 0 || (char*)pa < end || (uint64)pa >= PHYSTOP)
     panic("kfree");
@@ -54,13 +59,17 @@ kfree(void *pa)
   // Fill with junk to catch dangling refs.
   memset(pa, 1, PGSIZE);
 
-  r = (struct run*)pa;
+ r=(struct run*)pa;  
+  push_off();
+  int id=cpuid();
+  acquire(&kmem[id].lock);
+   r->next = kmem[id].freelist;
+  kmem[id].freelist = r;
+  release(&kmem[id].lock);
 
-  acquire(&kmem.lock);
-  r->next = kmem.freelist;
-  kmem.freelist = r;
-  release(&kmem.lock);
+  pop_off();
 }
+ 
 
 // Allocate one 4096-byte page of physical memory.
 // Returns a pointer that the kernel can use.
@@ -70,11 +79,52 @@ kalloc(void)
 {
   struct run *r;
 
-  acquire(&kmem.lock);
-  r = kmem.freelist;
-  if(r)
-    kmem.freelist = r->next;
-  release(&kmem.lock);
+  push_off();
+  int id = cpuid();
+
+  acquire(&kmem[id].lock);
+  r = kmem[id].freelist;
+  if(r) {
+    kmem[id].freelist = r->next;
+  }
+  else {
+    // alloc failed, try to steal from other cpu
+    int success = 0;
+    int i = 0;
+    for(i = 0; i < NCPU; i++) {
+      if (i == id) continue;
+      acquire(&kmem[i].lock);
+      struct run *p = kmem[i].freelist;
+      if(p) {
+        // steal half of memory
+        struct run *fp = p; // faster pointer
+        struct run *pre = p;
+        while (fp && fp->next) {
+          fp = fp->next->next;
+          pre = p;
+          p = p->next;
+        }
+        kmem[id].freelist = kmem[i].freelist;
+        if (p == kmem[i].freelist) {
+          // only have one page
+          kmem[i].freelist = 0;
+        }
+        else {
+          kmem[i].freelist = p;
+          pre->next = 0;
+        }
+        success = 1;
+      }
+      release(&kmem[i].lock);
+      if (success) {
+        r = kmem[id].freelist;
+        kmem[id].freelist = r->next;
+        break;
+      }
+    }
+  }
+  release(&kmem[id].lock);
+  pop_off();
 
   if(r)
     memset((char*)r, 5, PGSIZE); // fill with junk
